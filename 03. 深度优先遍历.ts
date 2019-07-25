2bf60c6336254cfb4c8300e8b1a9188799dd61af
fanzheng <montage_fz@sina.com>
2019-07-25 17:01:49 +0800

    - [新增] 深度优先

--- 03. 深度优先遍历.ts
+++ 03. 深度优先遍历.ts
@@ -1,4 +1,84 @@
 /**
  * Created by MonTage_fz on 2019/7/24
  */
-//
+// 深度优先遍历, 从一个顶点v出发, 然后依次从他各个未被访问的领节点出发深度优先搜索遍历图,
+// 直至途中所有和v有路径相同的点都北方问道
+let dom: Element = document.querySelector('.parent');
+
+interface IDeepTraversal {
+    (node: Element, nodeList?: Element[]): Element[]
+}
+
+// 在typeScript中, 当有类型使用object类型的时候, 使用interface来替换这个object
+interface IObject {
+    name: string,
+    children: object[]
+}
+
+
+interface IDeepTraversal_2 {
+    (list: IObject[]): any[]
+}
+
+
+const deepTraversal_1: IDeepTraversal = (node, nodeList = []) => {
+    if (!node) return;
+    nodeList.push(node);
+    let children = node.children;
+    for (let i = 0; i < children.length; i++) {
+        deepTraversal_1(children[i], nodeList);
+    }
+    return nodeList;
+};
+
+console.log(deepTraversal_1(dom));
+
+let arr = [
+    {
+        name: '1',
+        children: [
+            {
+                name: '1-1',
+                children: [
+                    {
+                        name: '1-1-1'
+                    }
+                ]
+            },
+            {
+                name: '1-2',
+                children: [
+                    {
+                        name: '1-2-1',
+                        children: [
+                            {
+                                name: '1-2-1-1'
+                            }
+                        ]
+                    },
+                    {
+                        name: '1-2-2'
+                    }
+                ]
+            },
+            {
+                name: '1-3'
+            }
+        ],
+        
+    }
+];
+
+const deepTraversal_2: IDeepTraversal_2 = (list) => {
+    let result = [];
+    let map = (i: { name: string, children: any[] }) => {
+        result.push(i.name);
+        i.children && i.children.forEach(v => map(v));
+    };
+    list.forEach(item => {
+        map(item);
+    });
+    return result;
+    
+};
+console.log(deepTraversal_2(arr));
--- dist/03. 深度优先遍历.js
+++ dist/03. 深度优先遍历.js
@@ -1,4 +1,64 @@
 /**
  * Created by MonTage_fz on 2019/7/24
  */
-//
+// 深度优先遍历, 从一个顶点v出发, 然后依次从他各个未被访问的领节点出发深度优先搜索遍历图,
+// 直至途中所有和v有路径相同的点都北方问道
+var dom = document.querySelector('.parent');
+var deepTraversal_1 = function (node, nodeList) {
+    if (nodeList === void 0) { nodeList = []; }
+    if (!node)
+        return;
+    nodeList.push(node);
+    var children = node.children;
+    for (var i = 0; i < children.length; i++) {
+        deepTraversal_1(children[i], nodeList);
+    }
+    return nodeList;
+};
+console.log(deepTraversal_1(dom));
+var arr = [
+    {
+        name: '1',
+        children: [
+            {
+                name: '1-1',
+                children: [
+                    {
+                        name: '1-1-1'
+                    }
+                ]
+            },
+            {
+                name: '1-2',
+                children: [
+                    {
+                        name: '1-2-1',
+                        children: [
+                            {
+                                name: '1-2-1-1'
+                            }
+                        ]
+                    },
+                    {
+                        name: '1-2-2'
+                    }
+                ]
+            },
+            {
+                name: '1-3'
+            }
+        ],
+    }
+];
+var deepTraversal_2 = function (list) {
+    var result = [];
+    var map = function (i) {
+        result.push(i.name);
+        i.children && i.children.forEach(function (v) { return map(v); });
+    };
+    list.forEach(function (item) {
+        map(item);
+    });
+    return result;
+};
+console.log(deepTraversal_2(arr));
