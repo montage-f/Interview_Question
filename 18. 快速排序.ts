1e1e4c548ff6d35a60eb233346a30977bcb8d80b
fanzheng <montage_fz@sina.com>
2019-08-09 15:08:50 +0800

    - [新增] 快速排序

--- 18. 快速排序.ts
+++ 18. 快速排序.ts
@@ -1,3 +1,27 @@
 /**
  * Created by MonTage_fz on 2019/8/1
  */
+let d = [8, 4, 90, 8, 34, 67, 1, 26, 17];
+
+const quicklySort = (arr): Array<number> => {
+    if (arr.length <= 1) return arr;
+    
+    // 取一个值作为标杆
+    let pivotIndex: number = Math.floor(arr.length / 2);
+    // 标杆
+    let pivot: number = arr.splice(pivotIndex, 1)[0];
+    // 定义两个数组, 一个用来存放 在比标高小的数, 一个用来存放比标杆大的数
+    let left: Array<number> = [];
+    let right: Array<number> = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return [...quicklySort(left), pivot, ...quicklySort(right)];
+};
+console.log(quicklySort(d));
+
--- dist/18. 快速排序.js
+++ dist/18. 快速排序.js
@@ -1,3 +1,25 @@
 /**
  * Created by MonTage_fz on 2019/8/1
  */
+var d = [8, 4, 90, 8, 34, 67, 1, 26, 17];
+var quicklySort = function (arr) {
+    if (arr.length <= 1)
+        return arr;
+    // 取一个值作为标杆
+    var pivotIndex = Math.floor(arr.length / 2);
+    // 标杆
+    var pivot = arr.splice(pivotIndex, 1)[0];
+    // 定义两个数组, 一个用来存放 在比标高小的数, 一个用来存放比标杆大的数
+    var left = [];
+    var right = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        }
+        else {
+            right.push(arr[i]);
+        }
+    }
+    return quicklySort(left).concat([pivot], quicklySort(right));
+};
+console.log(quicklySort(d));
