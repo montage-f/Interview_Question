2cfc9c335d9de23d6df7206907015fe2ccaf46d1
fanzheng <montage_fz@sina.com>
2019-08-09 14:20:17 +0800

    - [新增] list结构转成tree结构

--- dist/28. 将list结构转成tree结构.js
+++ dist/28. 将list结构转成tree结构.js
@@ -1,7 +1,30 @@
 /**
  * Created by MonTage_fz on 2019/8/6
  */
+var allList = [
+    { ids: 1, parentId: 0, name: 'Foods', },
+    { ids: 2, parentId: 1, name: 'Fruits' },
+    { ids: 3, parentId: 1, name: 'Vegetables' },
+    { ids: 4, parentId: 2, name: 'apple' },
+    { ids: 5, parentId: 2, name: 'orange' },
+    { ids: 6, parentId: 3, name: 'tomato' },
+    { ids: 7, parentId: 3, name: 'carrot' },
+    { ids: 8, parentId: 3, name: 'cabbage' },
+    { ids: 9, parentId: 3, name: 'potato' },
+    { ids: 10, parentId: 3, name: 'lettuce' },
+    { ids: 11, parentId: 0, name: 'Foods' },
+    { ids: 12, parentId: 11, name: 'Fruits' },
+    { ids: 13, parentId: 11, name: 'Vegetables' },
+    { ids: 14, parentId: 12, name: 'apple' },
+    { ids: 15, parentId: 12, name: 'orange' },
+    { ids: 16, parentId: 13, name: 'tomato' },
+    { ids: 17, parentId: 13, name: 'carrot' },
+    { ids: 18, parentId: 13, name: 'cabbage' },
+    { ids: 19, parentId: 13, name: 'potato' },
+    { ids: 20, parentId: 13, name: 'lettuce' }
+];
 var listToTree = function (myId, PId, list) {
+    // 判断是否有父节点
     var isPId = function (PId, list) {
         for (var i = 0; i < list.length; i++) {
             if (list[i][myId] === PId) {
@@ -14,13 +37,30 @@ var listToTree = function (myId, PId, list) {
     var nodes = [];
     for (var i = 0; i < list.length; i++) {
         var item = list[i];
+        // 如果没有父节点, 那么就是顶层节点
         if (!isPId(item[PId], list)) {
             nodes.push(item);
         }
     }
-    //
     var toDo = [];
     for (var i = 0; i < nodes.length; i++) {
         toDo.push(nodes[i]);
     }
+    while (toDo.length) {
+        var PNode = toDo.shift();
+        for (var i = 0; i < list.length; i++) {
+            var row = list[i];
+            if (row[PId] === PNode[myId]) {
+                if (PNode.children instanceof Array) {
+                    PNode.children.push(row);
+                }
+                else {
+                    PNode.children = [row];
+                }
+                toDo.push(row);
+            }
+        }
+    }
+    return nodes;
 };
+console.log(listToTree('ids', 'parentId', allList));
