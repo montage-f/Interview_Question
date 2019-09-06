ae627d58875ea0c1ce7aa00badd45c612532de9d
fanzheng <montage_fz@sina.com>
2019-09-06 11:17:28 +0800

    - [新增] 根据数字的顺序, 排列单词的位置

---
+++ codewars/01. 根据数字的顺序, 排列单词的位置.ts
@@ -0,0 +1,30 @@
+/**
+ * Created by MonTage_fz on 2019/9/6
+ */
+// 输出要求如下
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+    
+    // 自己的答案:
+const order = (sentence: string): string => {
+        if (!sentence) return sentence;
+        const list = sentence.split(' ');
+        const mapList = list.map((item) => {
+            const num = item.match(/[1-9]/)[0];
+            return {
+                name: item,
+                value: +num
+            };
+        });
+        return mapList.sort((a, b) => a.value - b.value).map(v => v.name).join(' ');
+    };
+console.log(order('abc1 bcd2 dad7 bcd4 bcd6'));
+
+
+    // 别人的答案
+const order2 = (sentence: string): string => {
+    return sentence.split(' ').sort((a, b) => +a.match(/\d/) - +b.match(/\d/)).join(' ');
+};
+console.log(order2('abc1 bcd2 dad7 bcd4 bcd6'));
+
