f1ac8eed3811bcdaac9d65dbacf04968580fb499
fanzheng <montage_fz@sina.com>
2019-10-21 20:20:56 +0800

    - [新增] js算法初体验 - [新增] jest测试

---
+++ JavaScript_算法/test/01. 反转字符串中的单词.test.js
@@ -0,0 +1,8 @@
+/**
+ * Created by MonTage_fz on 2019/10/21
+ */
+import reverseWord from '../01. 反转字符串中的单词';
+
+test('反转字符串中的单词', () => {
+    expect(reverseWord('Let\'s take LeetCode contest')).toBe('s\'teL ekat edoCteeL tsetnoc');
+});
